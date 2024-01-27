31e2df6fecbf666766f88eaefc9afec911dee23f
Sobhan Abedi <abedi.sobhan2000@gmail.com>
2024-01-27 21:05:55 +0330

    Done preprocessing using hazm library except common word elimination

--- main.py
+++ main.py
@@ -1,9 +1,36 @@
 import ujson as json
 import hazm as h
+from typing import List, Dict
+from os import path
+from dataclasses import dataclass
+
+@dataclass
+class PositionalPosting:
+    count: int
+    positions: List[int]
+
+
+@dataclass
+class WordEntry:
+    count: int
+    postings: List[PositionalPosting]
 
 
 def nmz(cnt: str) -> str:
     return normalizer.normalize(cnt)
+
+
+def remove_common_words(words: List[str]) -> List[str]:
+    # TODO: remove common words
+    return words
+
+
+def stem_list(words: List[str]) -> List[str]:
+    word_count = len(words)
+    stemmed_words = [''] * word_count
+    for i in range(word_count):
+        stemmed_words[i] = stemmer.stem(words[i])
+    return stemmed_words
 
 
 def main(filename: str):
@@ -11,17 +38,24 @@ def main(filename: str):
     data = json.load(f)
 
     for key, value in data.items():
+        doc_idx = int(key)
+        # Preprocessing:
         raw_cnt = value['content']
         # Normalize content
         nmz_cnt = nmz(raw_cnt)
+        words = h.word_tokenize(nmz_cnt)
+        if not path.exists("dict_" + filename):
+            pass
+        uncommon_words = remove_common_words(words)
+        final_words = stem_list(uncommon_words)
 
-        print(raw_cnt)
-        print(nmz_cnt)
-        break
+        # Generate Positional Inverted Index:
 
     f.close()
 
 
 if __name__ == '__main__':
     normalizer = h.Normalizer()
+    stemmer = h.Stemmer()
+    lemmatizer = h.Lemmatizer()
     main("data_500.json")
