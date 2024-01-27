c2c46af6c13fdd07d1ccbf16f9b7e0412d317f62
Sobhan Abedi <abedi.sobhan2000@gmail.com>
2024-01-27 21:31:23 +0330

    Naive implementation of positional inverted index generation

--- main.py
+++ main.py
@@ -13,11 +13,16 @@ class PositionalPosting:
 @dataclass
 class WordEntry:
     count: int
-    postings: List[PositionalPosting]
+    postings: Dict[int, PositionalPosting]
 
 
 def nmz(cnt: str) -> str:
     return normalizer.normalize(cnt)
+
+
+def remove_puncs(words: List[str]) -> List[str]:
+    # TODO: remove punctuation words
+    return words
 
 
 def remove_common_words(words: List[str]) -> List[str]:
@@ -44,12 +49,32 @@ def main(filename: str):
         # Normalize content
         nmz_cnt = nmz(raw_cnt)
         words = h.word_tokenize(nmz_cnt)
+        unpunc_words = remove_puncs(words)
         if not path.exists("dict_" + filename):
             pass
-        uncommon_words = remove_common_words(words)
+        uncommon_words = remove_common_words(unpunc_words)
         final_words = stem_list(uncommon_words)
 
         # Generate Positional Inverted Index:
+        pii: Dict[str, WordEntry] = {}
+        word_count = len(final_words)
+        for word_idx in range(word_count):
+            word = final_words[word_idx]
+            if word in pii:
+                entry = pii[word]
+                entry.count += 1
+                if doc_idx in entry.postings:
+                    posting = entry.postings[doc_idx]
+                    posting.count += 1
+                    posting.positions.append(word_idx)
+                else:
+                    entry.postings[doc_idx] = PositionalPosting(1, [word_idx])
+            else:
+                pii[word] = WordEntry(1, {doc_idx: PositionalPosting(1, [word_idx])})
+
+        for key, value in pii.items():
+            print(f"{key}: {value.postings[0].positions}")
+        break
 
     f.close()
 
